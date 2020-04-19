2bec80d418b61a1e47ae7b43b6058fd02b395849
Klairm <ivangardis@hotmail.com>
2020-04-19 10:58:24 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#FIXME : Add more distros
+echo "[1] Ubuntu / Debian / Debian-based [2] Arch / Manjaro / Arch-based\n"
+echo "Select your distro [1/2] -> " 
+read op
+if [ $op == 1 ];
+then
+	echo "Installing python and pip"
+	sudo apt install python python3 python3-pip python-pip
+	echo "Installing python dependencies"
+	pip3 install clint distro pyfiglet --user
+elif [ $op == 2 ];
+then
+	echo "Installing python and pip"
+	sudo pacman -S python3 python3-pip python-pip
+	echo "Installing python dependencies"
+	pip3 install clint distro pyfiglet --user
+fi
+
