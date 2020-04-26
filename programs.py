f001f55123580c0ec0fd1fff5d9fe479ea47ec43
Klairm <ivangardis@hotmail.com>
2020-04-26 22:33:57 +0200

    Debian installer for WINE

--- programs.py
+++ programs.py
@@ -41,7 +41,7 @@ def WINE(dist):
 		os.system("echo 'deb https://dl.winehq.org/wine-builds/debian/ {} main' | sudo tee -a /etc/apt/sources.list".format(debCodename))
 		os.system("sudo apt update")
 		os.system("sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386")
-		os.system("sudo apt install --install-recommends winehq-stable")
+		os.system("sudo apt install --install-recommends wine")
 
 		
 	else:
