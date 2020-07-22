0676de38b6227c7d8179abc64eea8815d3503cb7
Klairm <ivangardis@hotmail.com>
2020-07-22 20:35:21 +0200

    Added some dependencies

--- setup.sh
+++ setup.sh
@@ -9,13 +9,13 @@ read op
 if [ $op == 1 ];
 then
 	echo "Installing python and pip"
-	sudo apt install python3 python3-pip
+	sudo apt install python3 python3-pip curl jq git wget
 	echo "Installing python dependencies"
 	pip3 install clint distro pyfiglet --user
 elif [ $op == 2 ];
 then
 	echo "Installing python and pip"
-	sudo pacman -S python3 python3-pip python-pip
+	sudo pacman -S python3 python3-pip python-pip curl jq git wget
 	echo "Installing python dependencies"
 	pip3 install clint distro pyfiglet --user
 	sudo pip3 install clint
