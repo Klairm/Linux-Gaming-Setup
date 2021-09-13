e17d1b76cd66845fd802a50cfd3f2dc62e1fe8f1
Klairm <ivangardis@hotmail.com>
2021-09-13 23:40:41 +0200

    feat[ubuntu:feral_gamemode]: check if ubuntu version is 19 or higher to install gamemode

--- src/debian/feral_game_mode_installer.py
+++ src/debian/feral_game_mode_installer.py
@@ -1,4 +1,5 @@
 from src.feral_game_mode_installer import FeralGameModeInstaller as Base
+import distro
 
 
 class FeralGameModeInstaller(Base):
@@ -7,5 +8,9 @@ class FeralGameModeInstaller(Base):
         self.apt = apt
 
     def install(self):
-        self.apt.install(["meson", "libsystemd-dev", "pkg-config", "ninja-build", "git", "libdbus-1-dev", "libinih-dev"])
-        self.clone_feral_game_mode()
+        if int(distro.major_version()) >= 19 and distro.id() == "ubuntu":
+            self.apt.install(["gamemode"])
+        else:
+            self.apt.install(["meson", "libsystemd-dev", "pkg-config",
+                             "ninja-build", "git", "libdbus-1-dev", "libinih-dev"])
+            self.clone_feral_game_mode()
